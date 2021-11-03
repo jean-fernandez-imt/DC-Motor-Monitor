b8aa88d7d8e8da50932c1c52a740837f5ee9fd8b
JeanFernandez <jean.fernandez_imt@outlook.com>
2021-11-04 01:00:46 +0900

    Add Panel Buttons listeners

--- Motor_Panel/src/motor/panel/project/MotorPanel.java
+++ Motor_Panel/src/motor/panel/project/MotorPanel.java
@@ -1,12 +1,16 @@
 package motor.panel.project;
 
-import java.awt.BorderLayout;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.IOException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class MotorPanel {
+	
+	private static MotorRequests requests = new MotorRequests();
 
 	public static void main(String[] args) {
 		/// Creating the Frame
@@ -19,6 +23,38 @@ public class MotorPanel {
         JButton left = new JButton("Left");
         JButton right = new JButton("Right");
         JButton stop = new JButton("Stop");
+        
+        // Add button listeners
+        left.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		try {
+					requests.reqLeft();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+        	} 
+        });
+        
+        right.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		try {
+					requests.reqRight();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+        	} 
+        });
+        
+        stop.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		try {
+					requests.reqStop();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+        	} 
+        });
+        
         panel.add(left);
         panel.add(right);
         panel.add(stop);
